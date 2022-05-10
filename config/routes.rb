bb2ca10a384c7dc9cc84642372938dfdc226c10e
Marc-Antoine Blais <blaisma@live.fr>
2022-05-10 14:54:54 -0400

    task app is working as expected. need work on UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/', to: 'tasks#index'
+  get '/tasks', to: 'tasks#index', as: :tasks
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  get '/task/:id', to: 'tasks#show', as: :task
+  get 'task/:id/edit', to: 'tasks#edit', as: :edit
+  patch '/task/:id', to: 'tasks#update'
+  post '/tasks', to: 'tasks#create'
+  delete '/task/:id', to: 'tasks#destroy'
 end
